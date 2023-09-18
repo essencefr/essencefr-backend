74e387ff4e23bbd76689c94e6f2d1c39097a4b0d
younrx <git.youn@gmail.com>
2023-09-18 11:10:23 +0200

    use config module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * Main project file
  */
 
+const config = require('config');
 const express = require ('express');
 const app = express();
 
@@ -11,7 +12,6 @@ app.use(express.json());        // -> piece of middleware need to allow to pass 
 app.use('/api/test', test);     // -> links api endpoint '/api/test' to module defined in 'test.js'
 
 // Listerner:
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => { console.log(`Listening on port ${port}...`) });
+const server = app.listen(config.get('port'), () => { console.log(`Listening on port ${config.get('port')}...`) });
 
 module.exports = server;
