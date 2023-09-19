1a1ee5938ff8687873a8046d3a15c56bc566405d
younrx <git.youn@gmail.com>
2023-09-19 21:30:38 +0200

    use the server DB for tests instead of the local DB

--- startup/db.js
+++ startup/db.js
@@ -6,7 +6,8 @@ const config = require('config');
 const mongoose = require('mongoose');
 
 module.exports = function() {
-    const mongoURI = config.get('dbConnectionString');
+    let mongoURI = config.get('dbConnectionString');
+    if(process.env.NODE_ENV == 'test') mongoURI += '-test';  // use the test DB if we are in test mode (best way found so far to overwrite the env var value such as deefined in the config file)
     mongoose.connect(mongoURI)
     .then(() => console.log(`Connected to MongoDB: ${mongoURI}`));
     // .catch(err => console.error(`Cannot connect to MongoDB: ${mongoURI}`, err));  // -> no catch here so that the error is caught by the generic listener
