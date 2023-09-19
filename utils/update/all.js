1639212a10bd2776f51e748b6ee253944442b201
younrx <git.youn@gmail.com>
2023-09-19 23:04:24 +0200

    update TODO steps overview

--- utils/update/all.js
+++ utils/update/all.js
@@ -13,9 +13,14 @@ function processRawData(stationsDataRaw) {
     const stationsData = convertStationsFormat(stationsDataRaw);
     // TODO:
     // get all stations in stationsData with _id unknown in the database: store them in an array (let's say 'stationsDataNew')
-    // save them into the DB: call saveStations(stationsDataNew)
-    //  + is the brand unknown ? -> if yes, create new document into the brands collection
-    //  + is the fuel unknown ? -> if yes, create new document into the fuels collection
+    //  -> save stations data into stations documents in the DB: call saveStations(stationsDataNew)
+    //  -> create matching history documents and save prices data in it
+    //  -> is the brand unknown ? -> if yes, create new document into the brands collection
+    //  -> is the fuel unknown ? -> if yes, create new document into the fuels collection
     // get all stations in stationsData with _id already known in the database: store them in an array (let's say 'stationsDataUpdate')
-    // update the corresponding documents into the DB: call updateStations(stationsDataUpdate)
+    //  -> update the corresponding documents into the DB: call updateStations(stationsDataUpdate)
+    //  -> store fuel prices into matching history document
+    //
+    // Improvment idea: the lists of known/unkown stations' _id values can be stored in cache and only actualized when new station documents are created in order to improve performance
+    //                  -> no need to execute a query on the DB when there it is rare that a new station is being discovered
 };
