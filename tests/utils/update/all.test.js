5a222b4dc960a3e549202496611e7861c8177a5b
younrx <git.youn@gmail.com>
2023-09-20 22:30:35 +0200

    add filtering function

--- tests/utils/update/all.test.js
+++ tests/utils/update/all.test.js
@@ -2,17 +2,53 @@
  * Testing the generic update feature
  */
 
+const mongoose = require('mongoose');
+const { Station } = require('../../../models/station');
+const { processRawData } = require('../../../utils/update/all');
+const { filterStationsData } = require('../../../utils/update/all');
 const { convertStationsFormat } = require('../../../utils/convert');
 
 
 describe('generic update feature', () => {
-    test('processing raw data with missing fields should raise an error', () => {
-        const stationsDataRaw = [
-            { id: 1, name: 'a' },
-        ];
-        // ensure that a validation error is thrown:
-        expect(() => {
-            convertStationsFormat(stationsDataRaw);
-        }).toThrow(/validation error/i);
+
+    describe('main process', () => {
+        test('processing raw data with missing fields should raise an error', () => {
+            const stationsDataRaw = [
+                { id: 1, name: 'a' },
+            ];
+            // ensure that a validation error is thrown:
+            expect(() => {
+                processRawData(stationsDataRaw);
+            }).toThrow(/validation error/i);
+        });
+    });
+    
+    describe('filter feature', () => {
+        beforeEach(() => { server = require('../../../index'); });
+        afterEach( async () => {
+            server.close();
+            await Station.deleteMany({});
+        });
+        afterAll(() => { mongoose.disconnect(); });
+
+        test('an unknown station data object should be correctly filtered as new', async () => {
+            const { stationsDataRaw } = require('../../const');
+            const stationsData = convertStationsFormat(stationsDataRaw);
+            const stationsDataFiltered = await filterStationsData(stationsData);
+            expect(stationsDataFiltered).toBeDefined();
+            expect(stationsDataFiltered.stationsDataNew.length).toBe(1);
+            expect(stationsDataFiltered.stationsDataKnown.length).toBe(0);
+        });
+
+        test('an already known station data object should be correctly filtered as known', async () => {
+            const { stationsDataRaw } = require('../../const');
+            const { saveStations } = require('../../../utils/update/stations');
+            const stationsData = convertStationsFormat(stationsDataRaw);
+            await saveStations(stationsData);
+            const stationsDataFiltered = await filterStationsData(stationsData);
+            expect(stationsDataFiltered).toBeDefined();
+            expect(stationsDataFiltered.stationsDataNew.length).toBe(0);
+            expect(stationsDataFiltered.stationsDataKnown.length).toBe(1);
+        });
     });
 });
--- utils/update/all.js
+++ utils/update/all.js
@@ -4,6 +4,7 @@
 
 
 const { convertStationsFormat } = require('../convert');
+const { Station } = require('../../models/station');
 
 /**
  * Takes stations raw data and save/update documents in the DB
@@ -17,10 +18,44 @@ function processRawData(stationsDataRaw) {
     //  -> create matching history documents and save prices data in it
     //  -> is the brand unknown ? -> if yes, create new document into the brands collection
     //  -> is the fuel unknown ? -> if yes, create new document into the fuels collection
-    // get all stations in stationsData with _id already known in the database: store them in an array (let's say 'stationsDataUpdate')
-    //  -> update the corresponding documents into the DB: call updateStations(stationsDataUpdate)
+    // get all stations in stationsData with _id already known in the database: store them in an array (let's say 'stationsDataKnown')
+    // get all stations that have data different from the one in the DB (let's say 'stationsDataToUpdate'). This can be achieved by comparing hash values
+    //  -> update the corresponding documents into the DB: call updateStations(stationsDataToUpdate)
     //  -> store fuel prices into matching history document
     //
     // Improvment idea: the lists of known/unkown stations' _id values can be stored in cache and only actualized when new station documents are created in order to improve performance
-    //                  -> no need to execute a query on the DB when there it is rare that a new station is being discovered
+    //                  -> no need to execute a query on the DB when it is rare that a new station is being discovered
 };
+
+/**
+ * Separate a single array of stationData in two arrays:
+ *  - one containing the data related to stations unknown in the DB
+ *  - one containing the data related to stations already known in the DB
+ * @param {*} stationsData
+ */
+async function filterStationsData(stationsData) {
+    // create array with only station ids from input:
+    let listInputStationIds = [];
+    stationsData.forEach((stationObject) => {
+        listInputStationIds.push(stationObject._id);
+    });
+    // look for the stations already in the database:
+    const listKnownStationIds = await Station.find({ '_id': {$in: listInputStationIds}}).select('_id');
+    // create output:
+    let stationsDataFiltered = {
+        stationsDataNew: [],    // stations object with _id unknown in the DB
+        stationsDataKnown: []   // stations object with _id already known in the DB
+    };
+    // filter elements:
+    stationsData.forEach((stationObject) => {
+        if(listKnownStationIds.some(e => e._id == stationObject._id)) {  // i.e. if stationObject._id is in the list of ids known by the DB
+            stationsDataFiltered.stationsDataKnown.push(stationObject);
+        } else {            
+            stationsDataFiltered.stationsDataNew.push(stationObject);
+        };
+    });
+    return stationsDataFiltered;
+};
+
+module.exports.processRawData = processRawData;
+module.exports.filterStationsData = filterStationsData;
