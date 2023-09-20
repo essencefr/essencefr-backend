b9c17587761aae960a26ba58605868b963e322fb
younrx <git.youn@gmail.com>
2023-09-20 23:26:19 +0200

    hide sensitive data when logging DB connection string

--- startup/db.js
+++ startup/db.js
@@ -9,6 +9,6 @@ module.exports = function() {
     let mongoURI = config.get('dbConnectionString');
     if(process.env.NODE_ENV == 'test') mongoURI += '-test';  // use the test DB if we are in test mode (best way found so far to overwrite the env var value such as deefined in the config file)
     mongoose.connect(mongoURI)
-    .then(() => console.log(`Connected to MongoDB: ${mongoURI}`));
+    .then(() => console.log(`Connected to MongoDB: ${mongoURI.replace(/(?<=\/\/)\w*(?=:)/, '<username>').replace(/(?<=:)\w*(?=@)/, '<password>')}`));  // hide sensitive data before logging
     // .catch(err => console.error(`Cannot connect to MongoDB: ${mongoURI}`, err));  // -> no catch here so that the error is caught by the generic listener
 }
