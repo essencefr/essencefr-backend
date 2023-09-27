c8a139f2adc7b090a692423f384b2c19a8bffc0e
younrx <git.youn@gmail.com>
2023-09-27 19:41:57 +0200

    fix latitude/longitude order

--- tests/utils/fetch.test.js
+++ tests/utils/fetch.test.js
@@ -8,10 +8,10 @@ describe('fetch functions', () => {
     describe('fetchStations', () => {
         test('fetching stations with correct coordinates should return a known station data format', async () => {
             const { validateStationRaw } = require('../../models/station');
-            const longitude = 44.806;  // Pessac coordinates
-            const latitude = -0.631;
+            const latitude = 44.806;  // Pessac coordinates
+            const longitude = -0.631;
             // fetch data:
-            const stationRawObjectList = await fetchStations(longitude, latitude);
+            const stationRawObjectList = await fetchStations(latitude, longitude);
             // check format retrieved:
             expect(stationRawObjectList).toBeDefined();
             expect(stationRawObjectList.length).toBeGreaterThanOrEqual(1);
@@ -20,10 +20,10 @@ describe('fetch functions', () => {
         });
 
         test('fetching stations with coordinates out of France should return an empty body', async () => {
-            const longitude = 41.403;  // Barcelona coordinates
-            const latitude = 2.174;
+            const latitude = 41.403;  // Barcelona coordinates
+            const longitude = 2.174;
             // fetch data:
-            const stationRawObjectList = await fetchStations(longitude, latitude);
+            const stationRawObjectList = await fetchStations(latitude, longitude);
             // check format retrieved:
             expect(stationRawObjectList).toBeDefined();
             expect(stationRawObjectList.length).toBe(0);
--- utils/fetch.js
+++ utils/fetch.js
@@ -6,12 +6,12 @@
  * Function that retrieves the stations and gas prices 10km around a given location.
  * Calls API from https://swagger.2aaz.fr/
  * Data issues from https://www.prix-carburants.gouv.fr/rubrique/opendata/
- * @param {Number} longitude 
  * @param {Number} latitude 
+ * @param {Number} longitude 
  * @returns list of stations data in JSON format such as defined bu the government API
  */
-async function fetchStations(longitude, latitude) {
-    let response = await fetch(`https://api.prix-carburants.2aaz.fr/stations/around/${longitude},${latitude}?responseFields=FuelsPrices`, {
+async function fetchStations(latitude, longitude) {
+    let response = await fetch(`https://api.prix-carburants.2aaz.fr/stations/around/${latitude},${longitude}?responseFields=FuelsPrices`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
