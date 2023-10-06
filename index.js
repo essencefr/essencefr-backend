e2390d17ce2f777b1df05fa16af6140e939b6ed0
younrx <git.youn@gmail.com>
2023-10-06 22:49:25 +0200

    clean index.js

--- index.js
+++ index.js
@@ -2,18 +2,16 @@
  * Main project file
  */
 
+const logger = require('./logger');  // logging features + listener for uncaught exceptions and unhandled rejections
 const config = require('config');
 const express = require('express');
 const app = express();
 
-// require('./startup/logging')();     // init the logging features + listener for uncaught exceptions and unhandled rejections
-require('./logger');                // init the logging features + listener for uncaught exceptions and unhandled rejections
 require('./startup/routes')(app);   // init the routes
 require('./startup/db')();          // init the database connection
-// require('./services/cache');           // init the cache  // /!\ -> not needed here
 if (process.env.NODE_ENV == 'production') { require('./startup/prod')(app); }     // init the middlewares for production
 
 // Listerner:
-const server = app.listen(config.get('port'), () => { console.log(`Listening on port ${config.get('port')}...`) });
+const server = app.listen(config.get('port'), () => { logger.info(`Listening on port ${config.get('port')}...`) });
 
 module.exports = server;
