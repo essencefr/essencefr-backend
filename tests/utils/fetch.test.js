737d116b193e698767208ed4a65de4bcb8ca382d
younrx <git.youn@gmail.com>
2023-10-09 18:27:41 +0200

    rebase fetch on real gov api

--- tests/utils/fetch.test.js
+++ tests/utils/fetch.test.js
@@ -3,30 +3,17 @@
  */
 
 const { fetchStations } = require('../../utils/fetch');
+const { validateStationRaw } = require('../../utils/validate');
 
 describe('fetch functions', () => {
     describe('fetchStations', () => {
-        test('fetching stations with correct coordinates should return a known station data format', async () => {
-            const { validateStationRaw } = require('../../models/station');
-            const latitude = 44.806;  // Pessac coordinates
-            const longitude = -0.631;
-            // fetch data:
-            const stationRawObjectList = await fetchStations(latitude, longitude);
+        test('fetching stations should return the expected format', async () => {
+            const stationRawObjectList = await fetchStations();
             // check format retrieved:
             expect(stationRawObjectList).toBeDefined();
             expect(stationRawObjectList.length).toBeGreaterThanOrEqual(1);
             const { error } = validateStationRaw(stationRawObjectList[0]);
             expect(error).toBeUndefined();
-        });
-
-        test('fetching stations with coordinates out of France should return an empty body', async () => {
-            const latitude = 41.403;  // Barcelona coordinates
-            const longitude = 2.174;
-            // fetch data:
-            const stationRawObjectList = await fetchStations(latitude, longitude);
-            // check format retrieved:
-            expect(stationRawObjectList).toBeDefined();
-            expect(stationRawObjectList.length).toBe(0);
         });
     });
 });
--- utils/fetch.js
+++ utils/fetch.js
@@ -2,31 +2,20 @@
  * Functions fetching data on others servers APIs
  */
 
-const https = require('https');
 const { executeAndLogPerformance } = require('./timer');
 
 /**
- * Function that retrieves the stations and gas prices 10km around a given location.
- * Calls API from https://swagger.2aaz.fr/
- * Data issues from https://www.prix-carburants.gouv.fr/rubrique/opendata/
- * @param {Number} latitude 
- * @param {Number} longitude 
- * @returns list of stations data in JSON format such as defined bu the government API
+ * Function that retrieves the fuel prices for all known stations in France (can take up to 5 sec).
+ * More details at https://data.economie.gouv.fr/explore/dataset/prix-des-carburants-en-france-flux-instantane-v2/information/
+ * @returns list of stations data in JSON format such as defined by the government API
  */
-async function fetchStations(latitude, longitude) {
-    // process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;  // /!\ Note this should NOT be kept, this is a temporary fix because 'api.prix-carburants.2aaz.fr' has not renewed its certificate
-    let response = await fetch(`https://api.prix-carburants.2aaz.fr/stations/around/${latitude},${longitude}?responseFields=FuelsPrices`, {
+async function fetchStations() {
+    let response = await fetch(`https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/exports/json?limit=-1&timezone=Europe%2FParis&select=id%2C%20latitude%2C%20longitude%2C%20cp%2C%20adresse%2C%20ville%2C%20gazole_maj%2C%20gazole_prix%2C%20sp95_maj%2C%20sp95_prix%2C%20e85_maj%2C%20e85_prix%2C%20gplc_maj%2C%20gplc_prix%2C%20e10_maj%2C%20e10_prix%2C%20sp98_maj%2C%20sp98_prix`, {
         method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Range": "m=0-9999"  // distance around the point (max 10000 (10km))
-        },
-        agent: new https.Agent({
-            rejectUnauthorized: false,
-        })
+        headers: { "Content-Type": "application/json" }
     });
     let data = await response.json();
     return data;
 };
 
-module.exports.fetchStations = async (latitude, longitude) => { return await executeAndLogPerformance('Fetch stations data', 'info', async () => { return await fetchStations(latitude, longitude) }) };
+module.exports.fetchStations = async () => { return await executeAndLogPerformance('Fetch stations data', 'info', async () => { return await fetchStations() }) };
